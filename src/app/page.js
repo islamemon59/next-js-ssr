aa214af67d06d91fe852046859542059832c4a8d
islamemon59 <islamemon59@gmail.com>
2025-07-20 18:58:49 +0600

    next auth intall and login button added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import LoginButton from "./Components/LoginButton";
 
 export default function Home() {
   return (
@@ -49,6 +50,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <LoginButton/>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
